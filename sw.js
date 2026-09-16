dffe1b61d3bcc6798dc595d646bedbdd7e516362
Sander7rA <sander7ra@gmail.com>
2026-09-16 14:17:30 -0600

    loleros

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "academia-bans-v6";
+const CACHE = "academia-bans-v7";
 const ROOT = new URL("./", self.location.href);
 const FILES = [
   "./", "./index.html", "./css/global.css", "./css/login.css", "./css/portal.css", "./css/tasks.css",
